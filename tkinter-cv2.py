c633b943d828f891d9bf29101868a62de3677a1c
zhixiang98 <65327035+zhixiang98@users.noreply.github.com>
2021-07-04 19:28:27 +0800

    Update tkinter-cv2.py

--- tkinter-cv2.py
+++ tkinter-cv2.py
@@ -1,79 +1,114 @@
-import tkinter
+import tkinter as tk
 import cv2
 import PIL.Image, PIL.ImageTk
+import numpy as np
+
+class Camera():
+    def __init__(self,imgwidth =720, imgheight=576, webcam = True):
+        self.width = imgwidth
+        self.height =  imgheight
+        self.finalwidth = 720
+        self.finalheight = 576
+        self.pts1 = [[0,0],[200,0],[100,500],[250,500]]
+        self.min_area = 50
+        if webcam == True:
+            self.cap = cv2.VideoCapture(0)
+            self.cap.set(3,self.width)
+            self.cap.set(4,self.height)
 
 
-class MyVideoCapture:
 
-    def __init__(self, video_source=0):
-        self.vid = cv2.VideoCapture(video_source)
-        if not self.vid.isOpened():
-            raise ValueError("Unableto open video source", video_source)
+    def originalimage(self):
+        success, img = self.cap.read()
+        if success:
+            return img
 
-        # get video source width and height
-        self.width = self.vid.get((cv2.CAP_PROP_FRAME_WIDTH))
-        self.height = self.vid.get((cv2.CAP_PROP_FRAME_HEIGHT))
+    def Warp_perspective(self):  # pts1 to be in a list format [[],[],[],[]]
+        success, img = self.cap.read()
+        if success:
+            pts1 = np.float32(self.pts1)
+            pts2 = np.float32(
+                [[0, 0], [self.finalwidth, 0], [0, self.finalheight], [self.finalwidth, self.finalheight]])
+            self.matrix = cv2.getPerspectiveTransform(pts1, pts2)
+            warpimg = cv2.warpPerspective(img, self.matrix, (self.finalwidth, self.finalheight))
 
-    def get_frame(self):
-        if self.vid.isOpened():
-            ret, frame = self.vid.read()
-            if ret:
-                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            else:
-                return (ret, None)
+            return warpimg
 
-    def __del__(self):
-        if self.vid.isOpened():
-            self.vid.release()
+    def setWarp_Bounding_Box(self,coordinates):
+        self.__setattr__(self.pts1, coordinates)
+
+class App(tk.Tk,Camera):
+    def __init__(self,*args,**kwargs):
+        tk.Tk.__init__(self,*args,**kwargs)
+        Camera.__init__(self)                           #instantiating Camera class
 
 
-class App:
-    def __init__(self, window, window_title, video_source=0):
-        self.window = window
-        self.window.title(window_title)
-        self.video_source = video_source
-        self.mymenu = tkinter.Menu(self.window)
-        self.window.config(menu= self.mymenu)
-        self.delay = 15  # delay for update set in milliseconds
+        container = tk.Frame(self)                      #container is the main instance of Frame for our controller
 
-        # Create a menu bar
-        file_menu = tkinter.Menu(self.mymenu)
-        self.mymenu.add_cascade(label="File", menu=file_menu)
-        file_menu.add_command(label="New", command=self.file_new)
-        file_menu.add_command(label="Exit", command=self.window.quit)
+        container.pack(side = "top",fill = "both", expand = True)
+        container.grid_rowconfigure(0,weight = 1)
+        container.grid_columnconfigure(0,weight = 1)
 
-        # Create a new frame
-        self.file_new_frame = tkinter.Frame(self.window,width = 400,height = 400,bg = "red")
+        self.frames = {}                                  #creating a dictionary for all the different frames
 
-        # open video source
-        self.vid = MyVideoCapture(video_source)
+        for F in (OriginalView,WarpPerspective):
+            frame = F(container,self)
+            self.frames[F] = frame
+            frame.grid(row = 0, column = 0,sticky = "nsew")
 
-        # Create a canvas that fit above video source size
-        self.canvas = tkinter.Canvas(window, width=self.vid.width, height=self.vid.height * 2)
+        self.show_frame(OriginalView)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+
+
+class OriginalView(tk.Frame):
+    def __init__(self, parent, cont):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text = "Original Image")
+        label.pack(pady=10, padx=10)
+        self.cont = cont
+        self.delay = 50
+
+        self.canvas = tk.Canvas(self, width = cont.width, height = cont.height)
         self.canvas.pack()
-        self.update()
+        button = tk.Button(self,text = "press me",command = lambda: cont.show_frame(WarpPerspective))
+        button.pack()
+        self.update_canvas()
 
-        self.window.mainloop()
-
-    def update(self):
-        # Get a frame from video source
-        ret, frame = self.vid.get_frame()
-
-        if ret:
-            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
-
-        self.window.after(self.delay, self.update)
-
-    def hide_all_frames(self):
-        self.canvas.pack_forget()
-        self.file_new_frame.pack_forget()
-
-    def file_new(self):
-        self.hide_all_frames()
-        self.file_new_frame.pack(fill = 'both', expand =1)
+    def update_canvas(self):
+        self.img = self.cont.originalimage()
+        self.img = cv2.cvtColor(self.img,cv2.COLOR_BGR2RGB)
+        self.img = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(self.img))
+        self.canvas.create_image(0,0,image = self.img, anchor = 'nw')
+        self.after(self.delay,self.update_canvas)
 
 
 
-App(tkinter.Tk(), "Tkinter and OpenCV")
+class WarpPerspective(tk.Frame):
+    def __init__(self, parent, cont):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Original Image")
+        label.pack(pady=10, padx=10)
+        self.cont = cont
+        self.delay = 50
 
+        self.canvas = tk.Canvas(self, width=cont.width, height=cont.height)
+        self.canvas.pack()
+        button = tk.Button(self, text="press me", command=lambda: cont.show_frame(OriginalView))
+        button.pack()
+        self.update_canvas()
+
+    def update_canvas(self):
+        self.img = self.cont.Warp_perspective()
+        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+        self.img = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(self.img))
+        self.canvas.create_image(0, 0, image=self.img, anchor='nw')
+        self.after(self.delay, self.update_canvas)
+
+
+app = App()
+app.mainloop()
